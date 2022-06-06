084b268be171369d291907ca22eac7f10464b3ef
ShenSeanChen <seanchen9832@gmail.com>
2022-06-06 11:55:27 -0400

    Delete records with Flask

--- flasker/app.py
+++ flasker/app.py
@@ -118,7 +118,6 @@ def user(input_name):
 # Create Name Page
 @app.route('/name', methods=['GET', 'POST'])
 # Everytime you have a form, you are either getting or posting that form
-
 def name():
 	name = None
 	form = NamerForm()
@@ -153,8 +152,8 @@ def add_user():
 		form.email.data = ''
 		form.favorite_color.data = ''
 		flash("Your Form Was Submitted Successfully!")
-	our_users = Users.query.order_by(Users.date_added)
-		
+	
+	our_users = Users.query.order_by(Users.date_added)	
 	return render_template("add_user.html",
 		form=form,
 		name=name,
@@ -185,12 +184,38 @@ def update(id):
 	else:
 		return render_template("update.html",
 									form=form,
-									name_to_update=name_to_update)
+									name_to_update=name_to_update,
+									id=id)
 
+
+# /
+# Delete Database records
+@app.route('/delete/<int:id>')
+def delete(id):
+	user_to_delete = Users.query.get_or_404(id)
+
+	name="Sean"
+	form=UserForm()
+
+	try:
+		db.session.delete(user_to_delete)
+		db.session.commit()
+		flash("User Deleted Success!!")
 		
+		our_users = Users.query.order_by(Users.date_added)	
+		return render_template("add_user.html",
+			form=form,
+			name=name,
+			Users=Users,
+			our_users=our_users)
 
-
-
+	except:
+		flash("Whoops! There was a problem deleting user, try again plzzzz......")
+		return render_template("add_user.html",
+			form=form,
+			name=name,
+			Users=Users,
+			our_users=our_users)
 
 
 
