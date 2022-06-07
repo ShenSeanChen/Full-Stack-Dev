fd8059efb7493ae6a359679d306f1342ff609f65
ShenSeanChen <seanchen9832@gmail.com>
2022-06-07 10:24:39 -0400

    set up password with werkzeug

--- flasker/app.py
+++ flasker/app.py
@@ -6,6 +6,7 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 #################################
 # Create a Flask instance
@@ -47,6 +48,21 @@ class Users(db.Model): #inherit db.Model
 	email = db.Column(db.String(120), nullable=False, unique=True)
 	favorite_color = db.Column(db.String(120))
 	date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+	# Password
+	password_hash = db.Column(db.String(128))
+
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable attribute!')
+
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
 
 	# Create a string
 	def __repr__(self):
