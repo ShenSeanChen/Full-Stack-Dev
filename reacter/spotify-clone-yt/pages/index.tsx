2781a528c513533aa02dcfc83ba70ff506e166f1
ShenSeanChen <seanchen9832@gmail.com>
2022-07-24 11:55:46 -0700

    Spotify project

---
+++ reacter/spotify-clone-yt/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+
+// -- 1. npx create-next-app -e with-tailwindcss spotify-clone-yt
+// -- 2. index.js --> npm run dev
+
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+    </div>
+  )
+}
+
+export default Home
