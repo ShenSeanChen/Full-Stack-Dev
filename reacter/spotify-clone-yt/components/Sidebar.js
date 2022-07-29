d601fb1aafa430fe6d0809e3bccf2d1f29f8f96c
ShenSeanChen <seanchen9832@gmail.com>
2022-07-28 21:02:29 -0700

    spotify update

---
+++ reacter/spotify-clone-yt/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>I am a side bar.</div>
+  )
+}
+
+export default Sidebar
--- reacter/spotify-clone-yt/pages/index.tsx
+++ reacter/spotify-clone-yt/pages/index.tsx
@@ -1,22 +1,29 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-
-
+import Sidebar from '../components/Sidebar'
 
 // -- 1. npx create-next-app -e with-tailwindcss spotify-clone-yt
 // -- 2. index.js --> npm run dev
+// -- 3. heroicons: npm install @heroicons/react
 
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="">
+      <h1>This is a DOPE spotify 2.0 build</h1>
 
+      <main>
+        <Sidebar />
+        {/* Center */}
+      </main>
       
+
+      <div>
+        {/* Player */}
+      </div>
+
+
     </div>
   )
 }
