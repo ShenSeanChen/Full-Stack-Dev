37c0f63973c5ec4666e5f2913f6bb4daa5e38101
ShenSeanChen <seanchen9832@gmail.com>
2023-01-23 11:31:36 -0500

    update with dev using mongo and pirsma

---
+++ prismar/index.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.$connect()
+  
+    await prisma.user.create({
+      data: {
+        name: 'Rich',
+        email: 'hello@prisma.com',
+        posts: {
+          create: {
+            title: 'My first post',
+            body: 'Lots of really interesting stuff',
+            slug: 'my-first-post',
+          },
+        },
+      },
+    })
+
+    const author = await prisma.author.create({
+      data: {
+        name: "Shen Sean Chen"
+      }
+    })
+  
+    const allUsers = await prisma.user.findMany({
+      include: {
+        posts: true,
+      },
+    })
+    console.dir(allUsers, { depth: null })
+  }
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
